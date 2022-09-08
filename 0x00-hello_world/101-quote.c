3a5cc430acab02071226c8d836c307c8873cb67d
Braide Deinyibo Donald <111595638+Deinyibo@users.noreply.github.com>
2022-09-08 22:51:00 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - prints exactly "and that piece of art is useful" 
  * - Dora Korper, 2015-10-19",
  * followed by a new line, to the standard error.
- * Return: 0 (Zero) always
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
