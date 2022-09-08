2f99b6f9b7ecb77978caa224da6e1355c7fee619
Braide Deinyibo Donald <111595638+Deinyibo@users.noreply.github.com>
2022-09-08 22:29:02 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - prints exactly "and that piece of art is useful" - Dora Korper, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: 0 (Zero) always
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
