de68a1e3448553a0be8b8d42a297ce5c8fc6ff89
Braide Deinyibo Donald <111595638+Deinyibo@users.noreply.github.com>
2022-09-09 03:53:17 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main - the program starts executing here
+ *
  * Description - prints "with proper grammar, but the outcome is a piece of
  * art,"
- * Return - returns 0 and ends the program
+ * Return: returns 0 and ends the program
  */
 int main(void)
 {
