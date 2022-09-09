755e954d4c7f05d520b76d308f76e53fd2d3baf3
Braide Deinyibo Donald <111595638+Deinyibo@users.noreply.github.com>
2022-09-09 04:08:19 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,7 @@
  *
  * Return: Allways 0 (Success)
  */
-
-int main(void) 
+int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
