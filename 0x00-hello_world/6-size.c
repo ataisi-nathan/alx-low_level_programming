9e7e3095bc4eb7862ad9f4ddd012f1ff31604cde
Braide Deinyibo Donald <111595638+Deinyibo@users.noreply.github.com>
2022-09-09 04:06:19 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,6 @@
  *
  * Return: Allways 0 (Success)
  */
-
 int main(void) 
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
@@ -16,6 +15,5 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
-	
 	return (0);
 }
