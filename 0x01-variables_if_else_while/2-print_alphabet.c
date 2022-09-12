6da7067eec072ce7549baffad646671d53d6c4c6
Braide Deinyibo Donald <111595638+Deinyibo@users.noreply.github.com>
2022-09-12 16:32:40 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
 /**
  * main -Starting point
- *
  * prints lower case letters a-z
- *
  * Return: Always returns 0
  */
 int main(void)
-
 {
-        char alphabet;
-
-        for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-        {
-                      putchar(alphabet);
-        }
-
-        putchar('\n');
-
-        return (0);
-
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+putchar(alphabet);
+putchar('\n');
+return (0);
 }
