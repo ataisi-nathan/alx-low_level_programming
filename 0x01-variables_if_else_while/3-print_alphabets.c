f244e9903074ac7545e50abf0c38628b45b9bd69
Braide Deinyibo Donald <111595638+Deinyibo@users.noreply.github.com>
2022-09-12 16:11:51 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,30 +2,25 @@
 /**
  * main - Starting point
  *
+ * Prints lower case letters and uper case letters
+ *
  * Return: Always returns 0
  */
 int main(void)
 
 {
-char lower_case;
-char upper_case;
+        char lower_case;
+        char upper_case;
 
-for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
-{
-putchar(lower_case);
-}
+        for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+        
+        putchar(lower_case);
 
+        for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 
-for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
-{
-putchar(upper_case);
-}
+        putchar(upper_case);
 
+        putchar ('\n');
 
-
-
-putchar ('\n');
-
-return (0);
-
+        return (0);
 }
