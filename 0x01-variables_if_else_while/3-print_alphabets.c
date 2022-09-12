02c4f55daa06ac67b885c2b6e1973f0787806708
Braide Deinyibo Donald <111595638+Deinyibo@users.noreply.github.com>
2022-09-12 16:27:21 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 char lower_case;
-char upper_case;      
+char upper_case;
 for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 putchar(lower_case);
 for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
