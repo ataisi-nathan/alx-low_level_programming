cf854b0d23aef8f585efa3995b63d56ac850ccca
Braide Deinyibo Donald <dondein39@gmail.com>
2022-09-13 23:52:46 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,21 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * print_alphabet_x10 - Prints lower case alphabets 10x
+ * followed by a new line
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    print_alphabet_x10();
-    return (0);
+int round = 0;
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+    
+round++;
 }
