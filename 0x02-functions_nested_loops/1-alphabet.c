c603c28a619d567d18cffebed0e55d0be7e02817
Braide Deinyibo Donald <dondein39@gmail.com>
2022-09-13 23:35:32 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet - Print alphabet in lower case
- * followed by a new line
+ * main - check the code
  *
- * Return: Always return 0
+ * Return: Always 0.
  */
-void print_alphabet(void)
+int main(void)
 {
-char letter = 'a';
-while (letter <= 'z')
-{
-_putchar(letter);
-letter++;
-}
-_putchar('\n');
+    print_alphabet();
+    return (0);
 }
