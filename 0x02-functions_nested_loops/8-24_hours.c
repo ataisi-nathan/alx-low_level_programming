5d6611fb90d92e23b72f25da9411f7965cb4312c
Braide Deinyibo Donald <dondein39@gmail.com>
2022-09-14 02:32:33 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,30 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * jack_bauer - printf every second of the day in 00:00 format
  *
- * Return: Always 0.
+ * Return: there is no return just print to the console.
  */
-int main(void)
+
+void jack_bauer(void)
 {
-  jack_bauer();
-  return (0);
+int i, j, h1, h2, m1, m2;
+
+for (i = 0; i < 24; ++i)
+{
+for (j = 0; j < 60; ++j)
+{
+h1 = i / 10 + '0';
+h2 = i % 10 + '0';
+m1 = j / 10 + '0';
+m2 = j % 10 + '0';
+_putchar(h1);
+_putchar(h2);
+_putchar(':');
+_putchar(m1);
+_putchar(m2);
+_putchar('\n');
+}
+}
+
 }
