97cc3799e4aa1dca7781041c651b46e21cf954ee
Braide Deinyibo Donald <dondein39@gmail.com>
2022-09-19 21:30:07 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
+:set ff=dos
 /**
-*reset_to_98 Updates the value it points to to 98
-*@n: pointer to n to be updated
-* Return: Always return 0.
-*/
+ * reset_to_98 Updates the value it points to to 98
+ * @n: pointer to n to be updated
+ * Return: Always return 0
+ */
 void reset_to_98(int *n)
 {
 *n = 98;
